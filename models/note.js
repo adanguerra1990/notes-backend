34f69ce5ea9b1d3d704adf20dbf19e1bd391267a
adanguerra1990 <aj90guerra@gmail.com>
2024-02-29 15:10:00 -0400

    Estructura del proyecto

--- models/note.js
+++ models/note.js
@@ -1,18 +1,8 @@
+/**
+ * Esquema de Mongoose para las notas.
+ */
+
 const mongoose = require('mongoose')
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGOOSE_URI
-
-console.log('Conectando..', url)
-
-mongoose.connect(url)
-    .then(result => {
-        console.log('Conectado a mongoDB')
-    })
-    .catch((error) => {
-        console.log('Error de Conexion a mongoDB', error.message)
-    })
 
 const noteSchema = new mongoose.Schema({
     content: {
